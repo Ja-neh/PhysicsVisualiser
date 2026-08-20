835fac6b4c6ea158565fc561a2963110c28f88a8
AlteredFate <128898925+AlteredFate@users.noreply.github.com>
2026-08-20 09:04:46 +0200

    feat: Timer and Control Commands(play, pause, reset)
    Using RelayCommands for the Commands. Timer uses MAUI dispatcher and running at 60fps

--- PhysicsVisualiser/ViewModels/FlatSurfaceViewModel.cs
+++ PhysicsVisualiser/ViewModels/FlatSurfaceViewModel.cs
@@ -2,7 +2,9 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
+using Microsoft.Maui.Dispatching;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using PhysicsEngine;
 using PhysicsEngine.Bodies;
 using PhysicsEngine.Scenarios;
@@ -13,7 +15,7 @@ public partial class FlatSurfaceViewModel : ObservableObject
 {
     private Director _director;
 
-    // Physics default values
+    // Input properties
     [ObservableProperty]
     public partial double Mass {  get; set; }
     [ObservableProperty]
@@ -28,7 +30,7 @@ public partial class FlatSurfaceViewModel : ObservableObject
     public partial double Gravity{ get; set; }
 
 
-    // Runtime values to keep track of
+    // Runtime updating properies
     [ObservableProperty]
     public partial double CurrentTime { get; set; }
     [ObservableProperty]
@@ -46,20 +48,71 @@ public partial class FlatSurfaceViewModel : ObservableObject
     [ObservableProperty]
     public partial double CurrentNetForceX { get; set; }
 
-    // simulation
-    private bool CanPay;
-    private bool CanPause;
+    // simulation control
     private IDispatcherTimer? _timer;
-    public ICommand? PlayCommand;
-    public ICommand? PauseCommand;
-    public ICommand? ResetCommand;
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(PlayCommand))]
+    [NotifyCanExecuteChangedFor(nameof(PauseCommand))]
+    private partial bool IsRunning { get; set; }
+
+    public bool CanPlay => !IsRunning;
+    public bool CanPause => IsRunning;
+    public bool CanReset => true;
+
+    [RelayCommand(CanExecute = nameof(CanPlay))]
+    private void Play()
+    {
+        IsRunning = true;
+        if (_timer is null)
+        {
+            _timer = Dispatcher.GetForCurrentThread()?.CreateTimer() ?? throw new InvalidOperationException("Dispatcher.GetForCurrent thread called from background thread");
+            _timer.Interval = TimeSpan.FromSeconds(1.0 / 60.0); // 60 fps
+            _timer.Tick += OnTimerTick;
+        }
+        _timer.Start();
+    }
+
+    [RelayCommand(CanExecute = nameof(CanPause))]
+    private void Pause()
+    {
+        IsRunning = false;
+        if(_timer is null)
+        {
+            throw new InvalidOperationException("Pause called while timer is null"); // the button shouldn't even be enabled -- something is wrong if this is thrown
+        }
+        _timer.Stop();
+    }
+
+    [RelayCommand(CanExecute = nameof(CanReset))]
+    private void Reset()
+    {
+        IsRunning = false;
+        if( _timer is not null)
+        {
+            _timer.Stop();
+        }
+
+        CurrentTime = 0;
+    }
+
+
+    private void OnTimerTick(object? sender, EventArgs e)
+    {
+        if(_timer is null)
+        {
+            throw new InvalidOperationException("timer ticked while null");
+        }
+
+        CurrentTime += _timer.Interval.TotalSeconds;
+    }
 
 
     public FlatSurfaceViewModel()
     {
         FlatSurface flatSurface = new FlatSurface();
         _director = new Director(flatSurface);
-
+        IsRunning = false;
 
     }
 
