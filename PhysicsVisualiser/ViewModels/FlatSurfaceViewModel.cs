2a304c67dbaccbd076f3d7b90ede88114f06bf5b
AlteredFate <128898925+AlteredFate@users.noreply.github.com>
2026-08-20 04:07:17 +0200

    Add FlatSurfaceViewModel properties
    Add properties to the view model.
    Removed Surface inclination field and UI field since it's a flat surface scenario
    
    Helpers:
    - SetField function for updating fields.
    - onPropertyChanged function for invoking PropertyChanged event.

--- PhysicsVisualiser/ViewModels/FlatSurfaceViewModel.cs
+++ PhysicsVisualiser/ViewModels/FlatSurfaceViewModel.cs
@@ -18,7 +18,6 @@ public class FlatSurfaceViewModel : INotifyPropertyChanged
     private double _initialVelocityX = 0.0;
     private double _appliedForce = 0.0;
     private double _appliedForceAngle = 0.0;
-    private double _surfaceInclination = 0.0;
     private double _frictionCoefficient = 0.25;
     private double _gravity = Constants.earthGravitationalAcceleration;
 
@@ -40,6 +39,7 @@ public class FlatSurfaceViewModel : INotifyPropertyChanged
     public ICommand? PauseCommand;
     public ICommand? ResetCommand;
 
+
     public FlatSurfaceViewModel()
     {
         FlatSurface flatSurface = new FlatSurface();
@@ -48,5 +48,147 @@ public class FlatSurfaceViewModel : INotifyPropertyChanged
 
     }
 
-    
+    #region INPUT PROPERTIES
+    public double Mass
+    {
+        get => _mass;
+        set
+        {
+            SetField(ref _mass, value);
+        }
+    }
+
+    public double InitialVelocityX
+    {
+        get => _initialVelocityX;
+        set
+        {
+            SetField(ref _initialVelocityX, value);
+        }
+    }
+
+    public double AppliedForce
+    {
+        get => _appliedForce;
+        set
+        {
+            SetField(ref _appliedForce, value);
+        }
+    }
+
+    public double AppliedForceAngle
+    {
+        get => _appliedForceAngle;
+        set
+        {
+            SetField(ref _appliedForceAngle, value);
+        }
+    }
+
+    public double FrictionCoefficient
+    {
+        get => _frictionCoefficient;
+        set
+        {
+            SetField(ref _frictionCoefficient, value);
+        }
+    }
+    public double Gravity
+    {
+        get => _gravity;
+        set
+        {
+            SetField(ref _gravity, value);
+        }
+    }
+    #endregion
+
+
+    #region RUNTIME UPDATING PROPERTIES
+    public double CurrentTime
+    {
+        get => _currentTime;
+        set
+        {
+            SetField(ref _currentTime, value);
+        }
+    }
+
+    public double CurrentPositionX
+    {
+        get => _currentPositionX;
+        set
+        {
+            SetField(ref _currentPositionX, value);
+        }
+    }
+
+    public double CurrentVelocityX
+    {
+        get => _currentVelocityX;
+        set
+        {
+            SetField(ref _currentVelocityX, value);
+        }
+    }
+
+    public double CurrentAccelerationX
+    {
+        get => _currentAccelerationX;
+        set
+        {
+            SetField(ref _currentAccelerationX, value);
+        }
+    }
+
+    public double CurrentNormalForce
+    {
+        get => _currentNormalForce;
+        set
+        {
+            SetField(ref _currentNormalForce, value);
+        }
+    }
+
+    public double CurrentFrictionForce
+    {
+        get => _currentFrictionForce;
+        set
+        {
+            SetField(ref _currentFrictionForce, value);
+        }
+    }
+
+    public double CurrentWeightY
+    {
+        get => _currentWeightY;
+        set
+        {
+            SetField(ref _currentWeightY, value);
+        }
+    }
+
+    public double CurrentNetForceX
+    {
+        get => _currentNetForceX;
+        set
+        {
+            SetField(ref _currentNetForceX, value);
+        }
+    }
+    #endregion
+
+
+    // Helpers
+    private void SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return;
+        field = value;
+        OnPropertyChanged(propertyName);
+    }
+
+    private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
